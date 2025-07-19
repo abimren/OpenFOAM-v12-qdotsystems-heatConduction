fdbbefbe0a78d85a7bf025687129ff13eef888c7
Abdurrahman Imren <90402516+abimren@users.noreply.github.com>
2025-07-19 15:46:38 -0700

    Add files via upload

---
+++ 2_Steady1DCartHeatCondEqn_ConvectionBC_CodedMixed/plots/plot_OF_vs_analytical.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# Set Runtime Configuration in family groups
+plt.rc('figure',autolayout=True,figsize=(8,6))
+#plt.rc('font',family='Arial',size=20)
+plt.rc('font',size=18)
+# *********************************************************
+# Analytical - Left wall: Fixed T, Right wall: Convection
+h=10 #W/m^2
+Tinf=15
+k=10 #W/mK
+T1=100
+L=10
+x=np.linspace(0,L,100)
+T=h*(Tinf-T1)/(h*L+k)*x + T1
+plt.plot(x,T,label='Analytical') 
+# *********************************************************
+file='../postProcessing/sample/3/line_centreProfile.xy'
+data=np.loadtxt(file)
+print(file,data.shape,data[0])
+plt.plot(data[:,0],data[:,1],'^--',lw=2,label='OF',ms=10,mfc="none",markevery=10)
+plt.ylabel('Temperature (C)')
+plt.xlabel('Length (m)')
+plt.legend()
+plt.xlim((0,10))
+#plt.ylim((0,300))
+#plt.ylim(bottom=-1000) 
+plt.grid(ls='--')
+plt.savefig(f"temp.png")
+plt.show()
+# line  '-',':','--','-.'
+# color r,g,b,c,m,y,k,w
+# marker o,*,v,^,<,>,s,D,  d,x,X,+,P,p,h,1,2,3,4,|
+# marker color e.g., mfc="blue", mfc="none"
+# format: 'marker line color' e.g., 'D-k' 
+#plt.plot(results[:,0],results[:,1],'s-.',label='DVODE',ms=10,mfc="none",markevery=5)
